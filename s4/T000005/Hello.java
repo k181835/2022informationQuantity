53e3dadd593b35969c7a3dc18f16722da53d856f
k181835 <120450350+k181835@users.noreply.github.com>
2022-12-13 17:29:18 +0900

    Update Hello.java

--- s4/T000005/Hello.java
+++ s4/T000005/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Github(B181835)");
+        System.out.println("merge(B181835)");
     }
 }
 
